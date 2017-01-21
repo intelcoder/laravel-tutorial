70d816603d7ac77b5a6017df3093e1c677874c26
intelcoder <seongjunkim7162@gmail.com>
2017-01-20 23:56:24 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+let webpack = require('webpack');
+let path = require('path');
+
+module.exports = {
+    entry: './resources/assets/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public/js'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this nessessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                //materialize loader jquery, jquery.hammer preload
+                test: /materialize-css\/bin\//,
+                loader: 'imports?jQuery=jquery,$=jquery,hammerjs'
+            },
+        ]
+    },
+    plugins: [
+    ]
+};
